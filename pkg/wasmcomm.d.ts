973dbcbb68a9b3e83240cb5307b1faede644ada5
carrascomj <carrascomurielj@gmail.com>
2022-12-06 11:05:19 +0100

    refactor(nowork): pass receiver to a callback

---
+++ pkg/wasmcomm.d.ts
@@ -0,0 +1,94 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly main: (a: number, b: number) => number;
+  readonly wgpu_render_bundle_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_render_bundle_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_set_vertex_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_bundle_draw_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_bundle_draw_indexed_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_compute_pass_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_compute_pass_set_push_constant: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_compute_pass_insert_debug_marker: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_push_debug_group: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_pop_debug_group: (a: number) => void;
+  readonly wgpu_compute_pass_write_timestamp: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_begin_pipeline_statistics_query: (a: number, b: number, c: number) => void;
+  readonly wgpu_compute_pass_end_pipeline_statistics_query: (a: number) => void;
+  readonly wgpu_compute_pass_dispatch_workgroups: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_compute_pass_dispatch_workgroups_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_set_pipeline: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_bind_group: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_vertex_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_push_constants: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_draw: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_draw_indexed: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_draw_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_draw_indexed_indirect: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_multi_draw_indirect: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_render_pass_multi_draw_indexed_indirect: (a: number, b: number, c: number, d: number) => void;
+  readonly wgpu_render_pass_multi_draw_indirect_count: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_multi_draw_indexed_indirect_count: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly wgpu_render_pass_set_blend_constant: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_scissor_rect: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_set_viewport: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly wgpu_render_pass_set_stencil_reference: (a: number, b: number) => void;
+  readonly wgpu_render_pass_insert_debug_marker: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_push_debug_group: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_pop_debug_group: (a: number) => void;
+  readonly wgpu_render_pass_write_timestamp: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_begin_pipeline_statistics_query: (a: number, b: number, c: number) => void;
+  readonly wgpu_render_pass_end_pipeline_statistics_query: (a: number) => void;
+  readonly wgpu_render_bundle_set_index_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_bundle_push_debug_group: (a: number, b: number) => void;
+  readonly wgpu_render_bundle_pop_debug_group: (a: number) => void;
+  readonly wgpu_render_bundle_insert_debug_marker: (a: number, b: number) => void;
+  readonly wgpu_render_pass_set_index_buffer: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly wgpu_render_pass_execute_bundles: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h72f9aa86063fcb58: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3b54a89456dd3ac6: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h951214c9b7eb4ecf: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h628ef81211b94e58: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h23e6a42c999427ab: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc3237c971b844d24: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3b9cb5a94a9de3d8: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h2bbfca2c3d9e725e: (a: number, b: number, c: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1b86874c4efd6c58: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb0745542da8ef21b: (a: number, b: number) => void;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1a96928c9892a23d: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,57 @@
-use std::sync::mpsc::{channel, Receiver, Sender};
 use bevy::prelude::*;
 use serde::Deserialize;
+use std::sync::mpsc::{channel, Receiver, Sender};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::console;
+use web_sys::HtmlInputElement;
 
-fn main() {
-    let (tx, rx): (Sender<Example>, Receiver<Example>) = channel();
-
-    App::new()
-        .insert_resource(Msaa { samples: 4 })
-        .insert_resource(ReceiverWrapper {rx})
-        .insert_resource(Example {
-            field1: [1., 1., 1., 1.],
-        })
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                title: "shu".to_string(),
-                fit_canvas_to_parent: true,
-                ..default()
-            },
-            ..default()
-        }))
-        .add_system(listen_from_javascript)
-        .run();
-}
-
-#[derive(Resource)]
-struct ReceiverWrapper {
-    rx: Receiver<Example>,
-}
-
-fn listen_from_javascript(receiver: Res<ReceiverWrapper>) {
-    if let Ok(val) = receiver.rx.try_recv() {
-        info!("State received: {:?}", val);
-    }
-}
-
-#[derive(Deserialize, Debug, Default, Clone, PartialEq, Resource)]
+/// Data sent from callback through the channel.
+#[derive(Deserialize, Debug, Default, Clone, PartialEq)]
 pub struct Example {
     pub field1: [f32; 4],
 }
 
-#[wasm_bindgen]
-pub fn send_to_rust(val: &JsValue, tx: RustSender) {
-    let example: Example = val.into_serde().unwrap();
-    tx.field.send(example);
+fn main() {
+    // channel to communicate from WASM frontend to bevy runtime
+    let (tx, rx): (Sender<Example>, Receiver<Example>) = channel();
+    // the sender is owned by a WASM closure, called from the browser
+    let document = web_sys::window().unwrap().document().unwrap();
+    let upload_button = document
+        .get_element_by_id("upload")
+        .unwrap_throw()
+        .dyn_into::<web_sys::HtmlLabelElement>()
+        .unwrap_throw();
+    {
+        let target_brows = document
+            .get_element_by_id("fileb")
+            .unwrap()
+            .dyn_into::<HtmlInputElement>()
+            .unwrap_throw();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::Event| {
+            event.prevent_default();
+            if let Some(file_list) = target_brows.files() {
+                if let Some(Ok(example)) = file_list.get(0).map(|val| val.into_serde()) {
+                    console::log_1(&"Sending!".into());
+                    tx.send(example).unwrap();
+                }
+            }
+        }) as Box<dyn FnMut(_)>);
+        console::log_1(&"closure setup done!".into());
+        upload_button.set_onclick(Some(closure.as_ref().unchecked_ref()));
+    };
+
+    App::new()
+        // the receiver is passed to bevy as NonSend Resoruce
+        .insert_non_send_resource(rx)
+        .add_plugins(DefaultPlugins)
+        .add_system(listen_from_javascript)
+        .run();
 }
 
-#[wasm_bindgen]
-pub struct RustSender {
-    field: Sender<Example>,
+fn listen_from_javascript(receiver: NonSend<Receiver<Example>>) {
+    if let Ok(val) = receiver.try_recv() {
+        console::log_1(&"Receiving!".into());
+        info!("State received: {:?}", val);
+    }
 }
